dc5f32a2d5355f79af951d38c143c246a23f94bd
Rafael Brandao <r.augusto.brandao@gmail.com>
2016-08-13 00:15:17 -0300

    campos de edicao completados

--- public/js/directives/cliente/alterarcliente-controller.js
+++ public/js/directives/cliente/alterarcliente-controller.js
@@ -1,11 +1,36 @@
 var app = angular.module('RafaelShoes');
 
-app.directive('alterarcliente', ["$rootScope", "LoginService", function($rootScope, LoginService) {
+app.directive('alterarcliente', ["$rootScope", "LoginService", "localStorageService", "$http", "md5",
+  function($rootScope, LoginService, localStorageService, $http, md5) {
   return {
   	restrict: 'E',
   	link: function($scope){
 
       $scope.rootScope = $rootScope;
+
+      $scope.cliente = {
+        CPF_Cli: localStorageService.get('cpf'),
+        Nome_Cli: localStorageService.get('nome'),
+        Tel_Cel_Cli: localStorageService.get('celular'),
+        Tel_Res_Cli: localStorageService.get('telefone')
+      }
+
+      $scope.account = {};
+
+      $scope.salvar = function(){
+        if($scope.account.Senha){
+          $scope.account.Senha = md5.createHash($scope.account.Senha);
+        }
+        $scope.account.Login = $scope.cliente.Email;
+        var body = {
+          cliente: $scope.cliente,
+          account: $scope.account
+        }
+        $http.post($rootScope.api + 'v1/cliente/editar', body)
+          .success(function(data){
+            $rootScope.viewFlag = 9;
+          });
+      }
 
       $scope.logout = function(){
         LoginService.logout();
