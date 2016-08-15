14e4f2519db65f7fe8dd6dfbb1dbbad7555ad96d
Rafael Brandao <r.augusto.brandao@gmail.com>
2016-08-14 21:03:24 -0300

    cadastro de produto quase 100%

--- public/js/directives/produto/cadastrarproduto-controller.js
+++ public/js/directives/produto/cadastrarproduto-controller.js
@@ -1,7 +1,7 @@
 var app = angular.module('RafaelShoes');
 
-app.directive('cadastrarproduto', ["$rootScope", "LoginService", "localStorageService", 
-    function($rootScope, LoginService, localStorageService) {
+app.directive('cadastrarproduto', ["$rootScope", "LoginService", "localStorageService", "$http",
+    function($rootScope, LoginService, localStorageService, $http) {
   return {
   	restrict: 'E',
   	link: function($scope){
@@ -10,9 +10,39 @@ app.directive('cadastrarproduto', ["$rootScope", "LoginService", "localStorageSe
 
       $scope.nome = localStorageService.get('nome').split(" ")[0];
 
+      $scope.tamanhos = [{}];
+
+      $scope.adicionarTamanho = function(){
+        $scope.tamanhos.push({});
+      }
+
+      $scope.removerTamanho = function(tamanho){
+        for(var i = 0; i < $scope.tamanhos.length; i++){
+          if($scope.tamanhos[i] === tamanho){
+            $scope.tamanhos.splice(i, 1);
+          }
+        }
+      }
+
       $scope.cadastrar = function(){
-        // TODO: fazer sobreRafael
-        $rootScope.viewFlag = 1;
+        var body = {
+          Nome_Prod: $scope.Nome_Prod,
+          Desc_Prod: $scope.Desc_Prod,
+          Fornecedor_Prod: $scope.Fornecedor_Prod,
+          Peso_Prod: $scope.Peso_Prod,
+          Larg_Prod: $scope.Larg_Prod,
+          Comp_Prod: $scope.Comp_Prod,
+          Alt_Prod: $scope.Alt_Prod,
+          Preco_Prod: $scope.Preco_Prod,
+          Cor_Prod: $scope.Cor_Prod,
+          tamanhos: $scope.tamanhos
+        }
+
+        $http.post($rootScope.api + 'v1/produto/cadastrar', body)
+          .success(function(data){
+            window.alert("Produto cadastrado com sucesso!");
+            $rootScope.viewFlag = localStorageService.get('home');
+          })
       }
 
       $scope.logout = function(){
